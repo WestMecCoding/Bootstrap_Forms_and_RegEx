8412d75ceed6c9ab7c75df8a9a6675e920004afc
git <ryan.morales@west-mec.org>
2024-03-13 14:59:11 -0700

    added some code to the form validation to disable form submission on enter key

--- formValidation.js
+++ formValidation.js
@@ -1,30 +1,44 @@
+console.log("Hello World");
 (function () {
-  document.body.addEventListener("htmx:afterSwap", function (event) {
-    console.log("swap occured");
-    console.log(event.detail.target.id);
-    console.log(event.target);
-    console.log(event.target.querySelector("#email1"));
-    if (document.querySelector("#email1") != null && document.querySelector("[type='button']")) {
+  document.body.addEventListener("htmx:afterSwap", function () {
+    // Check if the necessary elements are present
+    if (
+      document.querySelector("#email1") &&
+      document.querySelector("#submit-button")
+    ) {
       initEmailValidation();
     }
   });
+
   function initEmailValidation() {
-    console.log("initing");
     const emailInput = document.getElementById("email1");
-    const submitButton = document.querySelector('button[type="button"]');
-    //   cache the emailInput and button
-    // set an event listener on the submitButton if it's not null
+    const submitButton = document.getElementById("submit-button");
+    const form = emailInput.closest("form"); // Get the closest form ancestor
+
+    // Prevent form submission with the Enter key
+    if (form) {
+      form.addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
+          event.preventDefault(); // Prevent form submission
+          return false;
+        }
+      });
+    }
+
+    // Add click event listener to the submit button
     if (submitButton) {
-      console.log("adding event listener to button");
       submitButton.addEventListener("click", validateEmail);
     }
-    // declare the validateEmail function
+
+    // Validation function
     function validateEmail() {
       const pattern = /^[a-zA-Z0-9._-]{1,16}@[a-zA-Z0-9-]+\.[a-zA-Z]{2,4}$/;
       if (pattern.test(emailInput.value)) {
         console.log("email is valid");
+        // Additional actions for a valid email, like submitting the form
       } else {
         console.log("invalid email");
+        // Handle invalid email input
       }
     }
   }
